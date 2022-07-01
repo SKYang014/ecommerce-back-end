947914174743799217e2b4138c509d11d33e3da9
poofy <skyang014@gmail.com>
2022-06-30 17:25:29 -0700

    fniished routes

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -2,11 +2,35 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    //  we added a user_id field that would hold the primary key 
+    //value of a user and post
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
